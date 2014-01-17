6b0317ed417ea60e54d528481054aee48a65bbb3
Thomas Bruun <thomas.bruun@gmail.com>
2014-01-17 16:14:04 +0100

    Add tests

--- test/i18next_crawler_test.js
+++ test/i18next_crawler_test.js
@@ -27,21 +27,13 @@ exports.i18next_crawler = {
     // setup here if necessary
     done();
   },
-  default_options: function (test) {
+  filters: function (test) {
+    // Test that all occurrences of i18next filters are correctly parsed and inserted into the translation file
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
-
-    test.done();
-  },
-  custom_options: function (test) {
-    test.expect(1);
-
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+    var actual = grunt.file.read('tmp/translations.json');
+    var expected = grunt.file.read('test/expected/translations.json');
+    test.equal(actual, expected, 'should parse all ng-i18next filter uses correctly');
 
     test.done();
   }
